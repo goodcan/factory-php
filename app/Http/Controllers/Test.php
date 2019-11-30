474d785255cba9aa6c2cdd46b40c56d8633af23c
cancan <1048042379@qq.com>
2019-12-01 02:56:01 +0800

    [upd] add db tet

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -4,11 +4,19 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class Test extends Controller
 {
     public function test() {
+        $data = DB::table('test')->get();
+//        foreach ($data as $item) {
+//            echo $item->id;
+//        }
+
         return response()->json([
-            'test' => 'hello world!'
+            'test' => 'hello world!',
+            'data' => $data
         ]);
     }
 }
